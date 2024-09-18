d5f1d1725605c06fd4f1a476b3d557efd6e55587
Abishek <abishek.swaminathan03@gmail.com>
2024-09-18 15:40:49 +0200

    servo_body working

--- arduino/Hubert_DemoApp_ROS2_ver1/Hubert_DemoApp_ROS2_ver1.ino
+++ arduino/Hubert_DemoApp_ROS2_ver1/Hubert_DemoApp_ROS2_ver1.ino
@@ -1,6 +1,3 @@
-#include <micro_ros_arduino.h>
-#include <yaml.h>
-
 /*
   Hubert robot test program.
 
@@ -14,14 +11,14 @@
   Note: correct port must be selected (ttyACM0, 1 or whatever)
   Same baud rate as in Arduino sketch (below)
   >> ros2 topic echo publisher
-  >> ros2 topic pub /servo_body std_msgs/Int16 data:\ <angle>\  
-  >> ros2 topic pub /servo_neck_pan std_msgs/Int16 data:\ <angle>\
-  >> ros2 topic pub /servo_neck_tilt std_msgs/Int16 data:\ <angle>\
-  >> ros2 topic pub /servo_shoulder std_msgs/Int16 data:\ <angle>\
-  >> ros2 topic pub /servo_elbow std_msgs/Int16 data:\ <angle>\
-  >> ros2 topic pub /servo_gripper std_msgs/Int16 data:\ <angle>\
+  >> ros2 topic pub /servo_body std_msgs/UInt16 data:\ <angle>\  
+  >> ros2 topic pub /servo_neck_pan std_msgs/UInt16 data:\ <angle>\
+  >> ros2 topic pub /servo_neck_tilt std_msgs/UInt16 data:\ <angle>\
+  >> ros2 topic pub /servo_shoulder std_msgs/UInt16 data:\ <angle>\
+  >> ros2 topic pub /servo_elbow std_msgs/UInt16 data:\ <angle>\
+  >> ros2 topic pub /servo_gripper std_msgs/UInt16 data:\ <angle>\
 
-  Replace <angle> with a write.microseconds value in approx. [500,2350], mid pos.: 1350
+  Replace <angle> with a write.microseconds value in approx. [500,2350], mid pos.: 1350/1420
   NOTE: Go back to NeutralPos. before terminating.
   
   created 23 Jun 2020
@@ -34,7 +31,6 @@
   http://www.arduino.cc/en/
 */
 
-#include <Arduino.h>
 #include <Servo.h>
 // #include <ros2arduino.h>
 #include <micro_ros_arduino.h>
@@ -50,17 +46,17 @@ std_msgs__msg__UInt16 Pwm;
 
 rcl_node_t node;
 
-//Allocator
-rcl_allocator_t allocator = rcl_get_default_allocator();
+// //Allocator
+rcl_allocator_t allocator;
 rclc_support_t support;
 
-// create executor
+// // create executor
 rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}} //printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;
-// #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+// // #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
-//Servos
+// //Servos
 Servo body;
 Servo headPan;
 Servo headTilt;
@@ -68,7 +64,7 @@ Servo shoulder;
 Servo elbow;
 Servo gripper;
 
-//Init position of all servos
+// //Init position of all servos
 const int servo_pins[] = {3, 5, 6, 9, 10, 11};
 
 const int pos_init[] = {1700, 1500, 2000, 2200, 1650, 1600};
@@ -80,10 +76,10 @@ const int pos_max[] = {2330, 2340, 2400, 2200, 2400, 2150};
 
 
 
-//Publisher
+// //Publisher
 rcl_publisher_t publisher;
 
-//Subscriber
+// //Subscriber
 rcl_subscription_t servo_body_ex;
 rcl_subscription_t servo_neck_pan;
 rcl_subscription_t servo_neck_tilt;
@@ -115,6 +111,8 @@ void servo_body_ex_cb(const void * msgin) {
   RCSOFTCHECK(rcl_publish(&publisher, &Pwm, NULL));
 	delay(20);
   }
+  Serial.println("YAhoooo");
+  Serial.println(Pwm.data);
   curr_pos[0] = now;
   delay(10);
 
@@ -263,114 +261,68 @@ void servo_gripper_ex_cb(const void * msgin) {
 
 
 
-// void setup() {
-
-//   Serial.begin(57600); // Starts the serial communication
-
-  //Init node
-	// hubert.initNode();
-  
-  //Subscriptions
-  // hubert.subscribe(sub_body);
-	// hubert.subscribe(sub_pan);
-	// hubert.subscribe(sub_tilt);
-	// hubert.subscribe(sub_shoulder);
-	// hubert.subscribe(sub_elbow);	
-  // hubert.subscribe(sub_gripper);
-
-  //Advertise
-  // hubert.advertise(publisher);
-
-	// //Attach each joint servo
-	// //and write each init position
-  // body.attach(servo_pins[0]);
-  // body.writeMicroseconds(pos_init[0]);
-  
-  // headPan.attach(servo_pins[1]);
-  // headPan.writeMicroseconds(pos_init[1]);
-  
-  // headTilt.attach(servo_pins[2]);
-  // headTilt.writeMicroseconds(pos_init[2]);
-
-  // shoulder.attach(servo_pins[3]);
-	// shoulder.writeMicroseconds(pos_init[3]);
-
-	// elbow.attach(servo_pins[4]);
-	// elbow.writeMicroseconds(pos_init[4]);
-	
-	// gripper.attach(servo_pins[5]);
-  // gripper.writeMicroseconds(pos_init[5]);
-
-  // //Initilize curr_pos and new_servo_val vectors
-  // byte i;
-  // for (i=0; i<(sizeof(pos_init)/sizeof(int)); i++){
-	// curr_pos[i] = pos_init[i];
-	// new_servo_val[i] = curr_pos[i];
-  // }
-
-	// delay(250);
-// }
-
 //loop or main ?? main replaces setup and loop
 //Recheck structure for loop and setup
 // int main()
 void setup() {
-  set_microros_transports();
-  Serial.begin(57600); // Starts the serial communication
+  
+  // Serial.begin(9600); // Starts the serial communication
+	set_microros_transports();
+  // // create init_options
+	allocator = rcl_get_default_allocator();
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-	// create init_options
-	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-
-	// create node
+	// // create node
 	
 	RCCHECK(rclc_node_init_default(&node, "hubert", "", &support));
 
-	// create publisher
+	// // create publisher
 	RCCHECK(rclc_publisher_init_default(
 		&publisher,
 		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt16),
 		"robot_state_publisher"));
 
-  // Update callbacks according to ros2 conventions
-	// create subscriber servo_body
+  // Serial.println("Check");
+  // // Update callbacks according to ros2 conventions
+	// // create subscriber servo_body
 	RCCHECK(rclc_subscription_init_default(
 		&servo_body_ex,
 		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt16),
     "servo_body"));
 
-	// create subscriber servo_neck_pan
+	// // create subscriber servo_neck_pan
 	RCCHECK(rclc_subscription_init_default(
 		&servo_neck_pan,
 		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt16),
 		"servo_neck_pan"));
 
-	// create subscriber servo_neck_tilt
+	// // create subscriber servo_neck_tilt
 	RCCHECK(rclc_subscription_init_default(
 		&servo_neck_tilt,
 		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt16),
 		"servo_neck_tilt"));
 	// create subscriber servo_shoulder
 	RCCHECK(rclc_subscription_init_default(
 		&servo_shoulder,
 		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt16),
 		"servo_shoulder"));
 	// create subscriber servo_elbow
 	RCCHECK(rclc_subscription_init_default(
 		&servo_elbow,
 		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt16),
 		"servo_elbow"));
 	
-	// create subscriber 6
+	// // create subscriber 6
 	RCCHECK(rclc_subscription_init_default(
 		&servo_gripper_ex,
 		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt16),
 		"servo_gripper"));
 
 	
@@ -382,8 +334,8 @@ void setup() {
   RCCHECK(rclc_executor_add_subscription(&executor, &servo_elbow, &recv_msg, &servo_elbow_cb, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &servo_gripper_ex, &recv_msg, &servo_gripper_ex_cb, ON_NEW_DATA));
 
-  //Attach each joint servo
-	//and write each init position
+  // //Attach each joint servo
+	// //and write each init position
   body.attach(servo_pins[0]);
   body.writeMicroseconds(pos_init[0]);
   
@@ -402,7 +354,7 @@ void setup() {
 	gripper.attach(servo_pins[5]);
   gripper.writeMicroseconds(pos_init[5]);
 
-  //Initilize curr_pos and new_servo_val vectors
+  // //Initilize curr_pos and new_servo_val vectors
   byte i;
   for (i=0; i<(sizeof(pos_init)/sizeof(int)); i++){
 	curr_pos[i] = pos_init[i];
@@ -412,19 +364,19 @@ void setup() {
 	
 }
 
-void loop (){
-
-  delay(100);
+void loop() {
+  
+  // Serial.println("Welcome");
   RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
   delay(250);
-  RCCHECK(rcl_subscription_fini(&servo_body_ex, &node));
-  RCCHECK(rcl_subscription_fini(&servo_neck_pan, &node));
-  RCCHECK(rcl_subscription_fini(&servo_neck_tilt, &node));
-  RCCHECK(rcl_subscription_fini(&servo_shoulder, &node));
-  RCCHECK(rcl_subscription_fini(&servo_elbow, &node));
-  RCCHECK(rcl_subscription_fini(&servo_gripper_ex, &node));
+  // RCCHECK(rcl_subscription_fini(&servo_body_ex, &node));
+  // RCCHECK(rcl_subscription_fini(&servo_neck_pan, &node));
+  // RCCHECK(rcl_subscription_fini(&servo_neck_tilt, &node));
+  // RCCHECK(rcl_subscription_fini(&servo_shoulder, &node));
+  // RCCHECK(rcl_subscription_fini(&servo_elbow, &node));
+  // RCCHECK(rcl_subscription_fini(&servo_gripper_ex, &node));
   
-  RCCHECK(rcl_publisher_fini(&publisher, &node));
+  // RCCHECK(rcl_publisher_fini(&publisher, &node));
 
-	RCCHECK(rcl_node_fini(&node));
+	// RCCHECK(rcl_node_fini(&node));
 }
