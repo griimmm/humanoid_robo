adca4e70b308e6d258a24b1757a28e4f8a2937b6
Rishikeshvishnu <rishikeshvishnusivakumar@gmail.com>
2024-10-14 14:18:46 +0200

    made changes to main.py to find rw coord

--- computer_vision/main.py
+++ computer_vision/main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import get_transform_function_for_vertical_plane_coord_to_base_frame_coord
+from detect_foam import find_foam
 
 # From camera calibration script
 camera_matrix = np.array([[631.13444093,  0.        ,321.42210038],
@@ -34,7 +35,7 @@ def main():
     cv2.namedWindow("Image")
 
     transform = None
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     while True:
         success, img = cap.read()
         img = get_undistorted_image(img, camera_matrix, distortion_coefficients)
@@ -51,23 +52,29 @@ def main():
         coord_saver = CoordinateSaver()
 
         # TEST:
-        cv2.setMouseCallback("Image", coord_saver.select_point)
-        while True:
-            cv2.imshow("Image", img)
-            key = cv2.waitKey(1)
-            if key == ord('q'):
-                break
-            elif key == ord('p'):
-                p_im = coord_saver.image_coords[-1]
-                cv2.circle(img,(p_im[0],p_im[1]),3,(255,0,0),-1)
-                print(f"World coord is {transform(p_im[0],p_im[1])}")
-
-        if len(coord_saver.image_coord_points)>0:
-            world_coords = []
-            for image_coord in coord_saver.image_coords:
-                world_coord = transform(image_coord[0], image_coord[1])
-                world_coords.append(world_coord)
-            send_points(world_coords)
+        # cv2.setMouseCallback("Image", coord_saver.select_point)
+        # while True:
+        #     cv2.imshow("Image", img)
+        #     key = cv2.waitKey(1)
+        #     if key == ord('q'):
+        #         break
+        #     elif key == ord('p'):
+        #         p_im = coord_saver.image_coords[-1]
+        #         cv2.circle(img,(p_im[0],p_im[1]),3,(255,0,0),-1)
+        #         print(f"World coord is {transform(p_im[0],p_im[1])}")
+        # if len(coord_saver.image_coord_points)>0:
+        #     world_coords = []
+        #     for image_coord in coord_saver.image_coords:
+        #         world_coord = transform(image_coord[0], image_coord[1])
+        #         world_coords.append(world_coord)
+        #     send_points(world_coords)
+        
+        points = find_foam(img)
+        world_coords = []
+        for image_coord in points:
+            world_coord = transform(image_coord[0], image_coord[1])
+            world_coords.append(world_coord)
+        send_points(world_coords)
 
     cv2.destroyAllWindows()
 
