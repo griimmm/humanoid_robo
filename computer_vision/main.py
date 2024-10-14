0352bdf31e6c66ffa733cd2c67f532ea4cba372c
aggehub <augustblanc4@gmail.com>
2024-10-14 13:56:24 +0200

    Added test for transform function

--- computer_vision/main.py
+++ computer_vision/main.py
@@ -16,7 +16,23 @@ def get_undistorted_image(img, cam_matrix, dist_coeff):
     undist_image = cv2.undistort(img, cam_matrix, dist_coeff, None, new_cam_matrix)
     return undist_image
 
+class CoordinateSaver:
+    def __init__(self):
+        self.image_coords = []
+
+    def select_point(self,event,x,y,flags,param):
+        """
+        Mouse callback function.
+        """
+        if event == cv2.EVENT_LBUTTONDOWN:#cv2.EVENT_LBUTTONDBLCLK:
+            self.image_coords.append((x,y))
+
+def send_points(world_coords: list):
+    pass
+
 def main():
+    cv2.namedWindow("Image")
+
     transform = None
     cap = cv2.VideoCapture(1)
     while True:
@@ -29,6 +45,29 @@ def main():
             break
         elif key == ord('h'):
             transform = get_transform_function_for_vertical_plane_coord_to_base_frame_coord.get_transform_function_for_vertical_plane_pixel_coord_to_base_frame_coord(img)
+            break
+    
+    if transform:
+        coord_saver = CoordinateSaver()
+
+        # TEST:
+        cv2.setMouseCallback("Image", coord_saver.select_point)
+        while True:
+            cv2.imshow("Image", img)
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+            elif key == ord('p'):
+                p_im = coord_saver.image_coords[-1]
+                cv2.circle(img,(p_im[0],p_im[1]),3,(255,0,0),-1)
+                print(f"World coord is {transform(p_im[0],p_im[1])}")
+
+        if len(coord_saver.image_coord_points)>0:
+            world_coords = []
+            for image_coord in coord_saver.image_coords:
+                world_coord = transform(image_coord[0], image_coord[1])
+                world_coords.append(world_coord)
+            send_points(world_coords)
 
     cv2.destroyAllWindows()
 
