c2ba7e0371336e63f5b119bb89f4d78b5dafb6bc
Abishek <abishek.swaminathan03@gmail.com>
2024-10-19 23:11:24 +0200

    small update

--- src/move_ee/move_ee/main.py
+++ src/move_ee/move_ee/main.py
@@ -6,20 +6,6 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Pose
 
-# From camera calibration script
-camera_matrix = np.array([[631.13444093,  0.        ,321.42210038],
-                          [  0.        ,632.04692079,219.63790101],
-                          [  0.        ,  0.        ,  1.        ]])
-
-# From camera calibration script
-distortion_coefficients = np.array([[ 0.05554114,-0.25243366,-0.01103603,-0.00042716, 0.36204737]])
-
-def get_undistorted_image(img, cam_matrix, dist_coeff):
-    h,w = img.shape[:2]
-    new_cam_matrix,roi = cv2.getOptimalNewCameraMatrix(cam_matrix, dist_coeff, (w,h), 1, (w,h))
-    undist_image = cv2.undistort(img, cam_matrix, dist_coeff, None, new_cam_matrix)
-    return undist_image
-
 class CoordinateSaver:
     def __init__(self):
         self.image_coords = []
@@ -42,25 +28,39 @@ class MinimalSubscriber(Node):
         self.publisher = self.create_publisher(Pose, 'cv_coord', 10)
         timer_period = 0.5  # seconds
         self.world_coord = []
+        # From camera calibration script
+        self.camera_matrix = np.array([[631.13444093,  0.        ,321.42210038],
+                          [  0.        ,632.04692079,219.63790101],
+                          [  0.        ,  0.        ,  1.        ]])
+
+        # From camera calibration script
+        self.distortion_coefficients = np.array([[ 0.05554114,-0.25243366,-0.01103603,-0.00042716, 0.36204737]])
+        self.cv_stuff()
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
+    def get_undistorted_image(self, img, cam_matrix, dist_coeff):
+        h,w = img.shape[:2]
+        new_cam_matrix, _ = cv2.getOptimalNewCameraMatrix(cam_matrix, dist_coeff, (w,h), 1, (w,h))
+        undist_image = cv2.undistort(img, cam_matrix, dist_coeff, None, new_cam_matrix)
+        return undist_image
+    
     def cv_stuff(self):
         cv2.namedWindow("Image")
 
         transform = None
-        cap = cv2.VideoCapture(1, cv2.CAP_DSHOW) # 0 or 1 usually
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # 0 or 1 usually
         
         while True:
-            success, img = cap.read()
-            img = get_undistorted_image(img, camera_matrix, distortion_coefficients)
+            _, img = cap.read()
+            img = self.get_undistorted_image(img, self.camera_matrix, self.distortion_coefficients)
             
             cv2.imshow("Image", img)
-            key = cv2.waitKey(1)
-            if key == ord('q'):
-                break
-            elif key == ord('h'):
-                transform = get_transform_function_for_vertical_plane_pixel_coord_to_base_frame_coord(img)
-                break
+            # key = cv2.waitKey(1)
+            # if key == ord('q'):
+            #     break
+            # elif key == ord('h'):
+            transform = get_transform_function_for_vertical_plane_pixel_coord_to_base_frame_coord(img)
+            break
         
         if transform:
             # coord_saver = CoordinateSaver()
@@ -86,11 +86,12 @@ class MinimalSubscriber(Node):
             points = find_foam(img, "tree-like")
             world_coords = []
             for image_coord in points:
-                world_coord = transform(image_coord[0], image_coord[1])
-                print(f"Coord is {world_coord}")
-                world_coords.append(world_coord)
+                self.world_coord = transform(image_coord[0], image_coord[1])
+                print(f"Coord is {self.world_coord}")
+                world_coords.append(self.world_coord)
             # send_points(world_coords)
-            return self.world_coord
+            # return self.world_coord
+            cv2.destroyAllWindows()
     
     def timer_callback(self):
         msg = Pose()        
@@ -109,7 +110,7 @@ def main(args=None):
 
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
-    cv2.destroyAllWindows()
+    
 
 if __name__=='__main__':
     main()
